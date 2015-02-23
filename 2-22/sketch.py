4248bf7f3a62a21fde6c5c6988bd7fe5f2d812cf
Jeff Ong <jonger4@gmail.com>
2015-02-22 22:58:24 -0500

    tags and lemmizes

--- 2-22/sketch.py
+++ 2-22/sketch.py
@@ -1,4 +1,54 @@
 import json
 import subprocess
+from subprocess import Popen, PIPE
+import os
+from nltk.stem.wordnet import WordNetLemmatizer
+import unirest
 
-subprocess.call("./../ccv/bin/cnnclassify ig/926318230906423186_22097384.jpg ../ccv/samples/image-net-2012.sqlite3", shell=True)
+f = open('../ccv/samples/image-net-2012.words')
+
+# strips the extra line break added by iterating over the file
+temp = [line[:-1] for line in f]
+
+words_list = []
+
+for line in temp:
+	words_list.append(line)
+
+f.close()
+
+def learn_deep(args):
+	p = subprocess.Popen(args, stdout=PIPE, shell=True)
+
+	data, err = p.communicate(input=None)
+
+	tags = data.split('\n')
+	tag_set = []
+
+	for t in tags:
+		if t:
+			tag, confidence = t.split()
+			if (float(confidence) > .1):
+				tag_set.append(words_list[int(tag)-1].split(', '))
+
+	return tag_set
+
+all_tags = []
+
+lmtzr = WordNetLemmatizer()
+
+for i in os.listdir('ig'):
+	if i.endswith(".jpg"): 
+		args = ['./../ccv/bin/cnnclassify ig/%s ../ccv/samples/image-net-2012.sqlite3' % i]
+		tags = learn_deep(args)
+
+		for t in tags:
+			if len(t) > 0:
+				lm = lmtzr.lemmatize(t[0])
+				all_tags.append(lm)
+		print all_tags
+		continue
+	else:
+		continue
+
+for tag in all_tags:
