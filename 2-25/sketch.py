eea67db0ab24112696b9fb59d5ce5cbf3fe35691
Jeff Ong <jonger4@gmail.com>
2015-02-26 09:21:18 -0500

    bot up

--- 2-25/sketch.py
+++ 2-25/sketch.py
@@ -6,40 +6,48 @@ creds = json.load(open('../configuration.json'))
 
 CONSUMER_KEY = creds["jffng_twit_key"]
 CONSUMER_SECRET = creds["jffng_twit_secret"]
-
 OAUTH_TOKEN = creds["jffng_twit_token"]
 OAUTH_TOKEN_SECRET = creds["jffng_twit_token_secret"]
+BOT_CONSUMER_KEY = creds["qualify_twit_key"]
+BOT_CONSUMER_SECRET = creds["qualify_twit_secret"]
+BOT_OAUTH_TOKEN = creds["qualify_twit_token"]
+BOT_OAUTH_TOKEN_SECRET = creds["qualify_twit_token_secret"]
 
-jffng_auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
-                           CONSUMER_KEY, CONSUMER_SECRET)
- 
-# jffng_user = twitter.Twitter(auth=jffng_auth)
-
-# bot_user = twitter.TwitterStream(auth=bot_auth)
-
-# twitter_stream = twitter.TwitterStream(auth=jffng_auth)
+jffng_auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
 twitter_userstream = twitter.TwitterStream(auth=jffng_auth, domain='userstream.twitter.com')
 
 statuses = twitter_userstream.user()
 
+qualify_auth = twitter.oauth.OAuth(BOT_OAUTH_TOKEN, BOT_OAUTH_TOKEN_SECRET, BOT_CONSUMER_KEY, BOT_CONSUMER_SECRET)
+
+qualify_user = twitter.Twitter(auth=qualify_auth)
+
 qualifiers = ['maybe', 'perhaps', 'supposedly', 'possibly', 'likely', 'conceivably', 'potentially']
 
 for t in statuses:
 	try:
-		tweet = t['text']
+		tw = t['text']
 		# print tweet
-		s = parsetree(t['text'], relations=True, lemmata=True)
+		s = parsetree(tw, relations=True, lemmata=True)
 		matches = search('NP will|be RB?+ PP|VP|ADJP|ADVP|NP', s)
 		if matches:
 			qualifier = qualifiers[random.randint(0,len(qualifiers)-1)]
-			status = tweet.split(matches[0].string)[0] + matches[0].string + ', ' + qualifier + ',' + tweet.split(matches[0].string)[1]
+
+			fragments = tw.split(matches[0].string)
+			
+			if fragments[1]:
+				status = fragments[0] + matches[0].string + ', ' + qualifier + ',' + fragments[1]
+			else:
+				status = fragments[0] + matches[0].string + ', ' + qualifier + '.'			
+			
 			status = status.replace('@', '')
+
 			if len(status) > 139:
 				print len(status)
 				status = status[:139]
 			print status
+			qualify_user.statuses.update(status=status)
+	
 	except KeyError:
-		continue
-	except IndexError:
 		continue
